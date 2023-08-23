5fa98911cf47b1de102abd0c9335a5f992af4b17
edgar <johnnyavila2021@u.northwestern.edu>
2023-08-23 16:36:39 -0500

    add projects to home page

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -8,7 +8,7 @@ const HireMe = () => {
       <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
         <CircularText className={`fill-dark animate-spin spin-slow`} />
         <Link
-          href="mailto:avila.johnny11@gmail.com"
+          href="mailto:johnnyavila2021@u.northwestern.edu"
           className={`flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark hover:border-2 md:w-12 md:h-12 md:text-[10px]`}
         >
           Hire Me
